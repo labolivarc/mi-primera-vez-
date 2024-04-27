4e8a4fb35812fc4b4a4d41fd75f1fd79ed4f149a
labolivarc <168300477+labolivarc@users.noreply.github.com>
2024-04-27 13:26:38 -0500

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,55 @@
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    . . # . .
+    . . . . .
+    . . . . .
+    `)
+basic.pause(500)
+let ax = 0
+basic.forever(function () {
+    ax = input.acceleration(Dimension.X)
+    if (ax > 818 && ax <= 1023) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . #
+            . . . . .
+            . . . . .
+            `)
+    } else if (ax > 409 && ax <= 818) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . # .
+            . . . . .
+            . . . . .
+            `)
+    } else if (ax > -409 && ax <= 409) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+    } else if (ax > -818 && ax <= -409) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . # . . .
+            . . . . .
+            . . . . .
+            `)
+    } else if (ax < -818 && ax <= -1023) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            # . . . .
+            . . . . .
+            . . . . .
+            `)
+    } else {
+    	
+    }
+})
